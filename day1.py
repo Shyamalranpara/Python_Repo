fbc73d741de65a623f4e809f5a265cbb8096369e
shyamalranpara <shyamalranpara7846@gmail.com>
2026-06-05 22:48:56 +0530

    day1.py

---
+++ day1.py
@@ -0,0 +1,7 @@
+#sum of two numbers
+
+print("enter two numbers")
+a=int(input())
+b=int(input())
+c=a+b
+print("the sum is",c)
---
+++ day2.py
@@ -0,0 +1,6 @@
+import math
+num=5.6
+
+x=math.floor(num)
+y=math.ceil(num)
+print(x,y)
