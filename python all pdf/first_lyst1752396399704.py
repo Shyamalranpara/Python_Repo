d564bc52aaf85623c63c9009c7d4ceb9135042ee
Shyamalranpara <162433756+Shyamalranpara@users.noreply.github.com>
2026-06-10 01:16:53 +0530

    Add files via upload

---
+++ python all pdf/first_lyst1752396399704.py
@@ -0,0 +1,39 @@
+# print("Welcome") #to print text
+# x=5
+# print(x) #to print value of a variable
+# y=4.5
+# print(x,y,20) #to print multiple values
+# print(x+y+1) #to print value of an expression
+
+#to print formatted text containing variables
+#x let us suppose you have two variable a and b, and you have to print a message "Sum of a and b is their sum"
+
+# a=5
+# b=4
+# print("Sum of %d and %d is %d"%(a,b,a+b))
+# print("Sum of",a,"and",b,"is",a+b)
+
+a=10
+b=20
+c=30
+print(a,b,c,sep='-',end='\t')
+
+print(a,end=' ')
+print(b,end='\n\n\n\n')
+print(c)
+print("Hello\rAB")
+print("\\")
+print("\"")
+
+"""
+Escape Sequences
+
+\n  New line
+\t  Tab pace
+\r  Carriage Return
+\b  Backspace
+\\  print \
+\"  print "
+\'  print '
+
+"""
