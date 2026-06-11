f6a9aae688571ee396abc309d9e33c15784b2a6c
Shyamalranpara <162433756+Shyamalranpara@users.noreply.github.com>
2026-06-12 02:07:37 +0530

    Add files via upload

---
+++ python all pdf/first_lyst1752396423044.py
@@ -0,0 +1,10 @@
+"""
+In Each Module
+    1) Instance Object
+    2) Function Object
+    3) Class Object
+
+Module | File with .py extension is module
+Package | Collection of modules (or packages)
+Library | Collection of Packages
+"""
